519b36ccb175648c5ec023e7340f60963ddf47de
HyokeunLee <hyok7575@gmail.com>
2017-02-06 13:51:40 +0900

    버블정렬 추가

---
+++ BubbleSort.java
@@ -0,0 +1,35 @@
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		/*
+		 *  BubbleSort Algorithm
+		 * 
+		 */
+		
+	 int [] array = { 1, -3, 0, 9, 7 };
+	 bubbleSort(array);
+	
+//	 for(int i=0; i<array.length; i++){
+//		 System.out.println(array[i]);
+//	 }
+ 
+	 for(int i : array){
+		 System.out.println(i);
+	 }
+	}
+	private static void bubbleSort(int array[]){
+		boolean isNeedsort = true; 
+		
+		while(isNeedsort){		
+			isNeedsort = false;
+			for(int index=0; index<array.length-1; index++){
+				if(array[index]>array[index+1]){
+					int temp = array[index+1];
+					array[index+1] = array[index];
+					array[index] = temp;
+					isNeedsort=true;
+				}
+			}
+		}
+	}
+}
